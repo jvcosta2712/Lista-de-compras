cadd55954af52c5ed2c55ba0950893fc374d0edd
joao <joao.costa@clint.digital>
2025-05-03 23:47:14 -0300

    wip

---
+++ src/scripts.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("meuFormulario");
+const nameItem = document.getElementById("name");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
